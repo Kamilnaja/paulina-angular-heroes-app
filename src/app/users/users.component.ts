64f9a9ce2aa051978786c02d6abb3c86d8a8d124
Kamil Naja <kamilnaja@gmail.com>
2023-11-16 21:25:20 +0100

    Update users.component.ts

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -9,13 +9,20 @@ import { Observable } from 'rxjs';
   styleUrls: ['./users.component.css'],
 })
 export class UsersComponent {
+  // mark as readonly, remove exclamation mark
+  // readonly selectedUser$ = new BehaviorSubject();
   selectedUser$!: Observable<User>;
 
+  // constructor should always be below fields
   constructor(private userService: UserService) {}
 
+  // readonly users 
   users$ = this.userService.getUsers();
 
+  // lowerCase
   OnSelect(userId: number): void {
+    // this is bad, you should not assign value to observable. 
+    // instead use behaviorSubject and run next()
     this.selectedUser$ = this.userService.getUserById(userId);
   }
 }
